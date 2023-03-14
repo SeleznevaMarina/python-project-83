32362718e7eea7bc35c865b8ae41cf2b06363be8
Selezneva Marina <petrukhina.marina2011@yandex.ru>
2023-03-14 19:53:33 +0300

    step 3

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL UNIQUE,
-    created_a timestamp
+    created_at timestamp
 );
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,21 +10,25 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect("dbname=analyzer user=saselezn")
 
 
-# def get_query(file):
+def get_data(path):
 
-#     with open(file, "r") as f:
-#         return f.read()
+    with open(path, "r") as f:
+        return f.read()
 
 
 def execute_query(connection, query):
-    # connection.autocommit = True
+    connection.autocommit = True
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        connection.commit()
         print("Query executed successfully")
     except psycopg2.Error as e:
         print(f"The error '{e}' occurred")
+
+
+
+create_table = get_data('database.sql')
+execute_query(conn, create_table)
 
 
 def execute_read_query(connection, query):
@@ -38,20 +42,29 @@ def execute_read_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
+def normalize_url(url):
+    url_parse = urllib.parse.urlparse(url)
+    normalized_url = url_parse._replace(fragment="").geturl()
+    return normalized_url
+
+
+def validate_url(url):
+
+
 @app.post('/urls')
 def add_url():
     url = request.form.get('url')
-    # url_record = urllib.parse.urlparse(url)
+    url = normalize_url(url)
+    errors = validate(url)
     insert_query = (
         f"INSERT INTO urls (name) VALUES (\'{url}\');"
         )
 
-    conn.autocommit = True
+    execute_query(conn, insert_query)
     select_query = (
         f"SELECT id FROM urls WHERE name = \'{url}\';"
         )
-    id = execute_read_query(conn, select_query)
-    print ('id=', id)
+    (id,) = execute_read_query(conn, select_query)[0]
 
     return redirect(f'/urls/{id}')
 
@@ -66,9 +79,11 @@ def get_url(id):
     select_query = (
         f"SELECT * FROM urls WHERE id = {id};"
         )
-    url_record = execute_query(conn, select_query)
-    # print(url_record)
-    return render_template('urls/show.html', url=url_record)
+    (id, name, created_at) = execute_read_query(conn, select_query)[0]
+
+    return render_template('urls/show.html',
+    id=id, name=name,
+    created_at=created_at)
 
 
 @app.route('/urls')
@@ -76,9 +91,11 @@ def get_urls():
     select_query = (
         f"SELECT * FROM urls;"
         )
-    url_records = execute_query(conn, select_query)
+    url_records = execute_read_query(conn, select_query)
 
     if url_records is None:
         url_records = []
 
-    return render_template('urls/index.html', urls=url_records)
+    return render_template('urls/index.html',
+    urls=url_records,
+    code=200)
